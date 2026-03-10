ba42ddf3aca6ec08faecb238847fdf8533281337
abdulmajeedalbalushi <codeline@example.com>
2026-03-10 08:39:30 +0400

    number ladder

---
+++ sprint 3/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int rows = 0; rows <= 5; rows++) {
+            for (int colums = 1; colums <= rows;colums++) {
+                System.out.print("\t"+colums);
+            } System.out.println();
+        }
+
+
+
+    }
+}
